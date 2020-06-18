75fce75fe49c39334223612d95dd1847e94e9c0c
Glitch (upperlinecode-favorite-books-object-practice-javascript-4) <none>
2020-06-18 14:05:14 +0000

    😘🌲 Checkpoint ./notes.js:3856783/263 ./index.html:3856783/12

---
+++ notes.js
@@ -0,0 +1,5 @@
+// When we are ready to practice, 
+// change the <script> tag in the html from notes.js
+// to objects.js
+console.log(`This is where we will practice with js objects!`)
+let taylor = ["Brown", "Brown", 27, "New York City", "Denver"]
